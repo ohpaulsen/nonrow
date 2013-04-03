128b9e26a2c104d447bae0fa202b77b2fff058c4
ohpaulsen <ole@ohpaulsen.no>
2013-04-03 17:15:39 +0200

    tok bort en klasse

--- class_game_action.cpp
+++ class_game_action.cpp
@@ -1,5 +1,9 @@
 #include "class_game_action.h"
-
+#include "class_cords.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 using namespace nonrow;
 
@@ -13,7 +17,26 @@ game_action::actiontype game_action::getType()
     return this->type;
 }
 
+
+
 void game_action::print()
 {
-
+    cout << "Write the cords you want to set your symbol eksample: 'a 1'" << endl;
+    string myString = "";
+    cin >> myString;
+    string l = "";
+    stringstream ss(myString); 
+    while ( ss >> l)
+        this->splitcords.push_back(l);
+   if(this->vectorLength() != 2)
+   {
+       cout << "invalid input" << endl;
+       this->print();
+   }
 }
+
+int game_action::vectorLength()
+{
+    return this->splitcords.size();
+}
+
--- class_game_action.h
+++ class_game_action.h
@@ -1,16 +1,20 @@
+#include "class_cords.h"
+#include <vector>
+#include <string>
 #ifndef ACTION
 #define ACTION
 
 namespace nonrow {
 
     class game_action {
-
     public:
     enum actiontype{put,pass};
     actiontype getType();
     void print();
     game_action();
+    int vectorLength();
     actiontype type;
+    std::vector<std::string> splitcords;
     };
 
 
